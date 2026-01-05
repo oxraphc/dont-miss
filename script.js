bf620b6e784aeb856ffca4f4953092e923ce808f
oxraphc <caesarravsanjani170@gmail.com>
2026-01-05 10:04:09 +0700

    Add location selector

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://api.myquran.com/v3/sholat/jadwal/d6baf65e0b240ce177cf70da146c8dc8/"
+const API_BASE_URL = "https://api.myquran.com/v3/sholat/jadwal/"
 const TIME_DELTA_THRESHOLD = 30; // Minutes
 const DHUHA_TIME_DELTA_THRESHOLD = 15; // Minutes
 
@@ -10,11 +10,13 @@ const deltaTimeLabel = document.getElementById('delta-time-label');
 const progressesBar = document.getElementById('progresses-bar');
 const progressesBarLabelContainer = document.getElementById('progresses-bar-label');
 const progressesBarLabelContainerContainer = document.getElementById('label-container');
+const locationSelector = document.getElementById('location');
 
 let savedDate = new Date().getDate();
 let prayerScheduleDownloaded = false;
 let nextPrayerIndex;
 let nextPrayerTimeDelta = 0;
+let selectedLocationIndex = locationSelector.selectedIndex;
 const prayerSchedule = [];
 // 0- Isya' (yesterday)
 // 1- Fajr
@@ -24,12 +26,26 @@ const prayerSchedule = [];
 // 5- Ashr
 // 6- Magrhib
 // 7- Isha'
+const locationID = [
+    'd6baf65e0b240ce177cf70da146c8dc8', // Tulungagung
+    'eda80a3d5b344bc40f3bc04f65b7a357', // Kota Kediri
+    '4734ba6f3de83d861c3176a6273cac6d', // Surabaya
+    '06138bc5af6023646ede0e1f7c1eac75', // Kota Malang
+    '577ef1154f3240ad5b9b413aa7346a1e', // Kota Yogyakarta
+    '74db120f0a8e5646ef5a30154e9f6deb', // Kota Semarang
+    '58a2fc6ed39fd083f55d4182bf88826d', // Kota Jakarta
+    'bd4c9ab730f5513206b999ec0d90d1fb', // Kota Tangerang
+    'cedebb6e872f539bef8c3f919874e9d7', // Kota Bekasi
+    '6a9aeddfc689c1d0e3b9ccc3ab651bc5' // Kota Denpasar
+]
 
 
 // Fetch and parse prayer schedules
 async function getPrayerSchedule() {
+    console.log("Fetching new schedule...");
     prayerScheduleDownloaded = false;
     prayerSchedule.length = 0; // Clear prayerSchedule array
+    const useLocationID = locationID[selectedLocationIndex];
 
     const now = new Date();
     const todayDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`; // YYYY-MM-DD
@@ -39,7 +55,7 @@ async function getPrayerSchedule() {
 
     // Fetch and parse yesterday's schedule
     try {
-        const response = await fetch(API_BASE_URL + yesterdayDate);
+        const response = await fetch(API_BASE_URL + useLocationID + '/' + yesterdayDate);
         if (response.ok) {
             const jsonResponse = await response.json();
             const jadwal = jsonResponse['data']['jadwal'][yesterdayDate];
@@ -51,11 +67,12 @@ async function getPrayerSchedule() {
         }
     } catch (e) {
         console.error(e);
+        return;
     }
 
     // Fetch and parse today's schedule
     try {
-        const response = await fetch(API_BASE_URL + 'today');
+        const response = await fetch(API_BASE_URL + useLocationID + '/' + 'today');
         if (response.ok) {
             const jsonResponse = await response.json();
             const jadwal = jsonResponse['data']['jadwal'][todayDate];
@@ -73,9 +90,11 @@ async function getPrayerSchedule() {
         }
     } catch (e) {
         console.error(e);
+        return;
     }
 
     prayerScheduleDownloaded = true;
+    console.log("New schedule fetched and parsed.")
 }
 getPrayerSchedule();
 
@@ -116,16 +135,6 @@ setInterval(() => {
 // }, 10);
 //  ========================================================================
 
-// 1. Update next prayer
-// 2. Calculate delta
-// 3. Have "Next prayer" and "Current prayer" with the former ranging 1 hour before adzan 
-//    and the latter ∞ hour after adzan
-// 4. Update time bar
-// 5. Fetch new time schedule when day changes
-// 6. Side arrow when progress goes beyond +-30m
-// 7. Add loading screen when waiting for data
-// 8. Refactor everything
-// TODO
 
 function updateClock(now) {
     const hours = now.getHours().toString().padStart(2, '0');
@@ -216,6 +225,25 @@ function updateView() {
     progressesBar.style.width = `calc(${progress}% + ${centerOffset}px)`;
     progressesBarLabelContainer.style.width = `calc(${progress}% + ${containerOffset}px + ${centerOffset}px)`;
 }
+
+
+// Detect locationSelector change
+locationSelector.addEventListener('change', () => {
+    selectedLocationIndex = locationSelector.selectedIndex;
+    console.log(`Selected location changed to ${locationSelector.value}`);
+
+    // Reset view while fetching
+    nextTitleElem.innerText = 'ㅤ';
+    nextPrayerElem.innerText = 'ㅤ';
+    nextPrayerTimeElem.innerText = 'Fetching schedule...';
+    deltaTimeLabel.innerText = '';
+    progressesBar.style.width = '0';
+    progressesBarLabelContainer.style.width = '0';
+    
+    // Fetch new schedule with new location
+    getPrayerSchedule();
+});
+
 
 function nextPrayerTimeDeltaPercent() {
     let useThreshold;
